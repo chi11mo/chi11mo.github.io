71018ffa03b94d985f0b9d36c5cbda766179d1ff
Dominic Wenzel <dwenzel@techfak.de>
2022-08-16 21:48:53 +0200

    deploy add

--- chi11mo.github.io/deploy.sh
+++ chi11mo.github.io/deploy.sh
@@ -26,7 +26,7 @@ echo ""
 echo "Committing changes to $(pwd)"
 git add .
 git commit -m "$msg"
-git push master
+git push origin master
 
 # Add this repos changes to git and commit/push. First 'cd' out of public
 cd ..
@@ -35,4 +35,4 @@ echo ""
 echo "Committing changes to $(pwd)"
 git add .
 git commit -m "$msg"
-git push master
+git push origin master
